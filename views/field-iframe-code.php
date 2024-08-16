6907877a544e20b52961a277634e6bbfce8e8bcc
mohamedKhalf <mohamed.khalaf@beetleware.net>
2024-08-16 19:22:06 +0300

    launch Woocommerce plugin latest version

---
+++ views/field-iframe-code.php
@@ -0,0 +1,2 @@
+<input type='text' name='cowpay_settings[IFRAME_CODE]' value='<?php echo esc_html($options['IFRAME_CODE']) ?>'>
+<p><?php esc_html_e('this frame code for Credit card method only', 'cowpay') ?></p>
